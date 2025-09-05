3d96496050a9c110451c2d0e6a146c6d1d3a9cb1
Abhinav Krishna <abhinavkrishnacr2020@gmail.com>
2025-09-05 13:32:52 +0530

    We can now read card title and content from the file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::{
     fs::File,
+    io::{BufRead, BufReader, Seek, SeekFrom},
     path::PathBuf,
     process::{Command, Stdio},
 };
@@ -8,8 +9,15 @@ use std::{
 mod fsrs;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Card {
+pub struct CardContent {
     id: u64,
+    title: String,
+    body: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Card {
+    content: CardContent,
     fsrs_params: fsrs::FSRSParams,
 }
 
@@ -22,6 +30,55 @@ pub fn data_path() -> anyhow::Result<PathBuf> {
         .unwrap_or(home))
 }
 
+pub fn find_card_content() -> anyhow::Result<Vec<CardContent>> {
+    // TODO: Support other searching commands, such as `grep -R` or `ag`
+    let grep_result = Command::new("rg")
+        .arg("-g")
+        .arg("*.{md,adoc,txt}")
+        .arg("-0b")
+        .arg("REVIEW:")
+        .stdin(Stdio::null())
+        .output()?;
+    let grep_result = String::from_utf8_lossy(&grep_result.stdout);
+    let data = grep_result
+        .split('\n')
+        .filter_map(|s| {
+            let (filename, rest) = s.split_once('\0')?;
+            let (byte_off_str, _content) = rest.split_once(":")?;
+
+            Some((PathBuf::from(filename), byte_off_str.parse().ok()?))
+        });
+
+    let mut acc = None;
+    for (cardpath, off) in data {
+        // This trickery is to avoid opening the same file multiple times
+        let (mut file, filename) = match acc {
+            // filepath == filename
+            Some((file @ _, filepath)) => (file, filepath),
+            _ => (BufReader::new(File::open(&cardpath)?), cardpath.clone()),
+        };
+
+        file.seek(SeekFrom::Start(off))?;
+        let mut line = String::new();
+        file.read_line(&mut line)?;
+
+        let mut content = String::new();
+        loop {
+            let mut next_line = String::new();
+            file.read_line(&mut next_line);
+            if next_line.starts_with("REVIEW:") || next_line == "---\n" || next_line == "" {
+                break;
+            }
+            content.push_str(&next_line);
+        }
+
+        println!("{line}<<<<\n{content}---");
+
+        acc = Some((file, filename));
+    };
+    Ok(vec![])
+}
+
 /// Intialize state, returning a list of flashcards
 pub fn initialize_cards() -> anyhow::Result<Vec<Card>> {
     let data_path = data_path()?;
@@ -30,24 +87,7 @@ pub fn initialize_cards() -> anyhow::Result<Vec<Card>> {
     let mut card_data = data_path.clone();
     card_data.push("cards.json");
     _ = card_data;
-
-    let actual_cards = Command::new("rg")
-        .arg("-g")
-        .arg("*.{md,adoc,txt}")
-        .arg("-0b")
-        .arg("REVIEW:")
-        .stdin(Stdio::null())
-        .output()?;
-    let content = String::from_utf8_lossy(&actual_cards.stdout);
-    let data: Vec<_> = content
-        .split('\n')
-        .filter_map(|s| {
-            let (filename, rest) = s.split_once('\0')?;
-            let (byte_off_str, content) = rest.split_once(":")?;
-            let byte_off: u64 = byte_off_str.parse().ok()?;
-            Some((filename, byte_off, content))
-        })
-        .collect();
+    let content = find_card_content();
 
     Ok(vec![])
 }
--- test/example.md
+++ test/example.md
@@ -1,5 +1,8 @@
 This card contains some example flashcards
 
+REVIEW: Where is the capital of India?
+New Delhi
+
 REVIEW: FSRS stands for _Free Spaced Repetition Scheduling_ Algorithm
 
 REVIEW: FSRS is based on the _DHP_ model of memory
